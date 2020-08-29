379ccdec42fe3fc269931e91903c1ef5442cf8f2
Ivan De Martino <ivan_demartino@outlook.com>
2020-08-29 10:50:21 -0300

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Ivan De Martino on 8/29/20.
+//  Copyright © 2020 Ivan De Martino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
